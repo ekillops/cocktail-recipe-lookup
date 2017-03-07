9460d8f8c392dcc4b6ee327adca4ec37c34a51e9
Erik Killops <erik.killops@gmail.com>
2017-03-07 08:58:22 -0800

    Refactor DrinkSearch into one query

--- src/CocktailRecipeLookup/Models/DrinkModel.cs
+++ src/CocktailRecipeLookup/Models/DrinkModel.cs
@@ -9,7 +9,7 @@ namespace CocktailRecipeLookup.Models
 {
     public class DrinkModel
     {
-        public static List<Drink> FindDrinksWithIngredients(List<string> ingredients)
+        public static List<Drink> FindDrinksWithExactIngredients(List<string> ingredients)
         {
 
             // Construct api query from ingredient list
@@ -45,6 +45,78 @@ namespace CocktailRecipeLookup.Models
             return removePartialMatches(resultDrinks.result, ingredients);
         }
 
+
+        public static List<Drink> FindDrinksWithIngredientsBroad(List<string> ingredients)
+        {
+            var bigDrinkList = new List<Drink>();
+
+            string queryString = "drinks/with/";
+
+            for(int i = 0; i < ingredients.Count; i++)
+            {
+                if (i != ingredients.Count - 1)
+                {
+                    queryString += ingredients[i] + "/or/";
+                }
+                else
+                {
+                    queryString += ingredients[i] + "/?apiKey=" + EnvironmentVariables.ADDbApiKey + "&pageSize=100";
+                }
+            }
+
+            RestClient client = new RestClient("http://addb.absolutdrinks.com/");
+            RestRequest request = new RestRequest(queryString);
+            RestResponse response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            DrinkQuery resultDrinks = JsonConvert.DeserializeObject<DrinkQuery>(response.Content);
+
+            bigDrinkList.AddRange(resultDrinks.result);
+
+            while (bigDrinkList.Count < resultDrinks.totalResult - 1)
+            {
+                var nextPage = GetNextPageDrinks(bigDrinkList.Count, queryString);
+                bigDrinkList.AddRange(nextPage);
+            }
+
+            return removePartialMatches(bigDrinkList, ingredients);
+        }
+
+        //public static List<Drink> FindDrinksWithIngredientsBroad(List<string> ingredients)
+        //{
+        //    var bigDrinkList = new List<Drink>();
+
+        //    foreach (string ingredient in ingredients)
+        //    {
+        //        string queryString = "drinks/with/" + ingredient + "/?apiKey=" + EnvironmentVariables.ADDbApiKey +"&pageSize=100";
+
+        //        RestClient client = new RestClient("http://addb.absolutdrinks.com/");
+        //        RestRequest request = new RestRequest(queryString);
+        //        RestResponse response = new RestResponse();
+
+        //        Task.Run(async () =>
+        //        {
+        //            response = await GetResponseContentAsync(client, request) as RestResponse;
+        //        }).Wait();
+
+        //        DrinkQuery resultDrinks = JsonConvert.DeserializeObject<DrinkQuery>(response.Content);
+
+        //        bigDrinkList.AddRange(resultDrinks.result);
+
+        //        while(bigDrinkList.Count < resultDrinks.totalResult - 1)
+        //        {
+        //            var nextPage = GetNextPageDrinks(bigDrinkList.Count, queryString);
+        //            bigDrinkList.AddRange(nextPage);
+        //        }
+        //    }
+
+        //    return removePartialMatches(bigDrinkList, ingredients);
+        //}
+
         private static List<Drink> removePartialMatches(List<Drink> drinkList, List<string> userIngredients)
         {
             List<string> standardIngredients = new List<string> { "ice-cubes", "simple-syrup", "bitters", "lemon", "lime", "orange", "maraschino-berry", "apple" };
@@ -62,7 +134,7 @@ namespace CocktailRecipeLookup.Models
                         break;
                     }
                 }
-                if(matching && !matches.Contains(drink))
+                if (matching && !matches.Contains(drink))
                 {
                     matches.Add(drink);
                 }
@@ -70,44 +142,10 @@ namespace CocktailRecipeLookup.Models
             return matches;
         }
 
-
-
-        public static List<Drink> FindDrinksWithIngredientsBroad(List<string> ingredients)
-        {
-            var bigDrinkList = new List<Drink>();
-
-            foreach (string ingredient in ingredients)
-            {
-                string queryString = "drinks/with/" + ingredient + "/?apiKey=" + EnvironmentVariables.ADDbApiKey +"&pageSize=100";
-
-                RestClient client = new RestClient("http://addb.absolutdrinks.com/");
-                RestRequest request = new RestRequest(queryString);
-                RestResponse response = new RestResponse();
-
-                Task.Run(async () =>
-                {
-                    response = await GetResponseContentAsync(client, request) as RestResponse;
-                }).Wait();
-
-                DrinkQuery resultDrinks = JsonConvert.DeserializeObject<DrinkQuery>(response.Content);
-
-                bigDrinkList.AddRange(resultDrinks.result);
-
-                while(bigDrinkList.Count < resultDrinks.totalResult - 1)
-                {
-                    var nextPage = GetNextPageDrinks(bigDrinkList.Count, queryString);
-                    bigDrinkList.AddRange(nextPage);
-                }
-            }
-
-            return removePartialMatches(bigDrinkList, ingredients);
-        }
-
-
         private static List<Drink> GetNextPageDrinks(int retrievedSoFar, string query)
         {
             RestClient client = new RestClient("http://addb.absolutdrinks.com/");
-            RestRequest request = new RestRequest(query + "&start="+ (retrievedSoFar + 1));
+            RestRequest request = new RestRequest(query + "&start=" + (retrievedSoFar + 1));
 
             RestResponse response = new RestResponse();
 
@@ -119,16 +157,6 @@ namespace CocktailRecipeLookup.Models
             DrinkQuery resultDrinks = JsonConvert.DeserializeObject<DrinkQuery>(response.Content);
             return resultDrinks.result;
         }
-
-
-
-
-
-
-
-
-
-
 
         public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
         {
