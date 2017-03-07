6c0295fb472d27fb3da65c7db8e5ed7324f4f10c
ekillops <erik.killops@gmail.com>
2017-03-06 18:36:20 -0800

    Delete EnvironmentVariables.cs

--- src/CocktailRecipeLookup/Models/EnvironmentVariables.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CocktailRecipeLookup.Models
-{
-    public static class EnvironmentVariables
-    {
-        public static readonly string ADDbApiKey = "a1cb8a4eab11496e9d11828f5ad31287";
-    }
-}
